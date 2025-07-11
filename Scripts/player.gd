5de5492d9f2ef3715fc539c2d961e16267999a1e
Guilherme Campos <129324360+GPinkyWill@users.noreply.github.com>
2025-07-11 12:39:19 -0300

    Initial commit

---
+++ Scripts/player.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+@export var acceleration = 512
+@export var max_velocity = 128
+@export var friction = 350
+@export var gravity = 200
+@export var jump_force = 128
+@export var max_fall_velocity = 120
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+@export var jump_height : float
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1
+
+
+func _physics_process(delta: float) -> void:
+	apply_gravity(delta)
+	velocity.y += _get_gravity() * delta
+	#Movimentação direita e esquerda
+	var input_axis = Input.get_axis("ui_left","ui_right")
+	if is_moving(input_axis):
+		apply_acceleration(delta, input_axis)
+	else:
+		apply_friction(delta)
+	if Input.is_action_just_pressed("ui_accept"):
+		jump()
+	update_animations(input_axis)
+	move_and_slide()
+
+func is_moving(input_axis):
+	return input_axis != 0
+
+func _get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump():
+	velocity.y = jump_velocity
+
+func apply_gravity(delta):
+	#Aplicação de gravidade para queda do personagem
+	if not is_on_floor():
+		velocity.y = move_toward(velocity.y, max_fall_velocity, gravity * delta)
+
+
+func apply_acceleration(delta, input_axis):
+	if is_moving(input_axis) :
+		velocity.x = move_toward(velocity.x,input_axis * max_velocity,acceleration * delta)
+	
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x,0,friction * delta)
+		
+#func jump_check ():
+#	if is_on_floor():
+#		if Input.is_action_just_pressed("ui_accept"):
+#			velocity.y -= jump_force
+#	else: 
+#		if Input.is_action_just_released("ui_accept") and velocity.y < -jump_force/2:
+#			velocity.y = -jump_force/2
+
+func update_animations (input_axis):
+	if is_moving(input_axis):
+		animation_player.play("run")
+		sprite_2d.scale.x = sign(input_axis)
+	else:
+		animation_player.play("idle")
+	if not is_on_floor():
+		animation_player.play("jump")
+		
