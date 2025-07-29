66c323c18f2af9ebde3a5fdfbff5326c7e2abdeb
Guilherme Campos <129324360+GPinkyWill@users.noreply.github.com>
2025-07-29 13:09:38 -0300

    Walking Enemy
    Adição de inimigo que anda para direita e esquerda, detecta paredes e muda de direção e que também detecta bordas para não cair

---
+++ Scripts/walking_enemy.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@export var speed = 15.0
+@export var turn_at_ledges = true
+
+
+var gravity = 200.0
+var direction = 1.0
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var floor_cast: RayCast2D = $FloorCast
+
+
+
+
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if is_on_wall():
+		turn_around()
+	if is_at_ledge():
+		turn_around()
+	moving()
+	
+	move_and_slide()
+
+
+func moving():
+	velocity.x = direction * speed
+	sprite_2d.scale.x = direction
+	
+	
+func is_at_ledge():
+	return is_on_floor() and not floor_cast.is_colliding() 
+	
+func turn_around():
+	direction *= -1.0
+	floor_cast.scale.x = direction
+	
