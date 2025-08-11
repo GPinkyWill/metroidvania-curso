a7f0ebcae4e68a20dd10c87ecebb7a140d6223e4
Guilherme Campos <129324360+GPinkyWill@users.noreply.github.com>
2025-08-11 15:23:19 -0300

    Door path
    Dentro de Path, em Scenes, um recurso é criado para as trocas de portas deixando mais especificado para qual porta voltar.
    
    Além disso um timer foi adicionado pra dar um pequeno delay, assim o player é reposicionado antes de ativar a porta, evitando que, caso a porta entre 2 níveis estejam no mesmo local, o player não apenas teleporte.

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -2,16 +2,22 @@ class_name Door
 extends Area2D
 
 
-@export_file("*.tscn") var new_level_path
 
+@export_file("*.tscn") var new_level_path
+@export var connection : DoorConnection
+
+
+@onready var timer: Timer = $Timer
 @onready var right_cast: RayCast2D = $RightCast
 @onready var left_cast: RayCast2D = $LeftCast
 
+var active = false
 
 func _physics_process(delta: float) -> void:
 	var player = MainInstances.player as Player
 	if !player is Player: return
-	if overlaps_body(player):
+	if !active: return
+	if overlaps_body(player) and new_level_path:
 		var player_direction = sign(player.velocity.x)
 		var direction = get_direction()
 		if player_direction == direction:
@@ -21,3 +27,7 @@ func get_direction():
 	if left_cast.is_colliding(): return -1
 	if right_cast.is_colliding(): return 1
 	return 0
+
+
+func _on_timer_timeout() -> void:
+	active = true
--- Scripts/world.gd
+++ Scripts/world.gd
@@ -19,4 +19,6 @@ func change_level(door : Door):
 	var doors = get_tree().get_nodes_in_group("doors")
 	for found_door in doors:
 		if found_door == door: continue
-		player.global_position = found_door.global_position
+		if found_door.connection != door.connection: continue
+		var y_offset = player.global_position.y - door.global_position.y
+		player.global_position = found_door.global_position + Vector2(0, y_offset)
